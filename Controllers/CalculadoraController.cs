11cb46b1e069bb5741cc9a31077e70f07acdeaff
unknown <miguel.tirado0929@gmail.com>
2019-09-24 00:32:37 -0500

    Se agrego los menus, las vistas y la multiplicacion

--- Controllers/CalculadoraController.cs
+++ Controllers/CalculadoraController.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tarea3.Models;
 
+
 namespace Tarea3.Controllers
 {
+    
     public class CalculadoraController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
@@ -20,13 +23,17 @@ namespace Tarea3.Controllers
             return View();
         }
 
-   public String    suma(int n1, int n2)
-        {  var n3=n1+n2;
-            string mensaje="El resultado de sumar"+n1+"+"+n2+"Es igual a"+n3;
+   public String    suma(int n1=5,  int n2=4)
+        {  int n3=n1+n2;
+            string mensaje="El resultado de sumar "+ n1  +"+" +n2+"Es igual a  "+n3;
             return mensaje;
         }
        
-        
+        public String    multiplicacion(int n1=5,  int n2=4)
+        {  int n3=n1*n2;
+            string mensaje="El resultado de multiplicar"+ n1  +"+" +n2+"Es igual a  "+n3;
+            return mensaje;
+        }
     
 }
 }
---
+++ Models/CalculadoraModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Tarea3.Models
+{
+    public class CalculadoraModel
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+}
+}
